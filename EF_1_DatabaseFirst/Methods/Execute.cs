425d31f8d5769f63089d30253ce7f54d68f2b659
Altan Emre <altan_emre_1989@hotmail.com>
2025-02-20 21:34:45 +0300

    LINQ

---
+++ EF_1_DatabaseFirst/Methods/Execute.cs
@@ -0,0 +1,85 @@
+﻿using EF_1_DatabaseFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_1_DatabaseFirst.Methods
+{
+    internal class Execute
+    {
+        NorthwindContext db = new NorthwindContext();
+
+        internal Order OrderGetById(int id)
+        {
+            //Order order = db.Orders.Include(i=> i.Customer).Where(i => i.OrderId == id).FirstOrDefault() ?? new Order();
+            Order order = db.Orders.Include(i => i.Customer).FirstOrDefault(i => i.OrderId == id) ?? new Order();
+
+            return order;
+        }
+
+        internal List<Order> GetOrders()
+        {
+            return db.Orders.OrderByDescending(i => i.OrderDate).ToList();
+        }
+
+        //select ProductID,SUM(Quantity) from [Order Details] group by ProductID
+        internal List<ProductSumQuantity> ProductGetQuantity()
+        {
+            var models = db.OrderDetails
+                           .GroupBy(i => i.ProductId) // Group by ProductId
+                           .Select(g => new ProductSumQuantity
+                           {
+                               ProductId = g.Key, // ProductId from the group
+                               SumQuantity = g.Sum(i => i.Quantity) // Sum of Quantity in the group
+                           })
+                           .ToList();
+
+            return models;
+        }
+
+        internal List<Product> ProductsGetByCategory(int categoryId = 0)
+        {
+            #region 1.Yöntem
+            //if (categoryId > 0)
+            //{
+            //    return db.Products.Where(i => i.CategoryId == categoryId).ToList();
+            //}
+
+            //else
+            //{
+            //    return db.Products.ToList();
+            //}
+
+            #endregion
+
+            #region 2.Yöntem
+            //if (categoryId > 0)
+            //{
+            //    return db.Products.Where(i => i.CategoryId == categoryId).ToList();
+            //}
+
+            //return db.Products.ToList();
+            #endregion
+
+            var products = db.Products.AsQueryable(); //SQL sorgusunu tamamlanmadı olarak işaretledi. Cümlenin sonuna virgül koydu.
+
+            if (categoryId > 0)
+            {
+                products = products.Where(i => i.CategoryId == categoryId);
+            }
+
+            return products.ToList();
+            
+        }
+
+    }
+
+    class ProductSumQuantity
+    {
+        public int ProductId { get; set; }
+        public int SumQuantity { get; set; }
+    }
+}
--- EF_1_DatabaseFirst/Program.cs
+++ EF_1_DatabaseFirst/Program.cs
@@ -14,24 +14,94 @@ EF Çeşitleri
  
  ******** DATABASE FIRST ********
  Scaffold-DbContext "Server=DESKTOP-58CMK8T\SQLDERS;Database=Northwind;Trusted_Connection=True; TrustServerCertificate=true;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities
- */
 
-using EF_1_DatabaseFirst.Entities;
-using Microsoft.EntityFrameworkCore;
 
 NorthwindContext db = new NorthwindContext();
 
-//foreach (var item in db.Products.Include(i=>i.Category).ToList())
-//foreach (var item in db.Products.Include("Category").ToList())
-//{
-//    Console.WriteLine(item.ProductId + " " + item.ProductName+" "+item.Category.CategoryName);
-//}
+        //foreach (var item in db.Products.Include(i=>i.Category).ToList())
+        foreach (var item in db.Products.Include("Category").ToList())
+        {
+            Console.WriteLine(item.ProductId + " " + item.ProductName + " " + item.Category.CategoryName);
+        }
 
-var products = db.Products.Where(i => i.CategoryId > 3).ToList();
 
-foreach (var item in products)
+
+ */
+
+
+
+using EF_1_DatabaseFirst.Entities;
+using EF_1_DatabaseFirst.Methods;
+using Microsoft.EntityFrameworkCore;
+
+class Program
 {
-    Console.WriteLine(item.ProductName+" "+item.CategoryId);
+    static void Main()
+    {
+        Execute execute = new Execute();
+
+        #region Kendisine parametre olarak verilen OrderId datası ile sipariş bilgisini döndüren method
+
+        //Order order = execute.OrderGetById(10249);
+
+        //Console.WriteLine(order.OrderId+" "+order.CustomerId+" "+order.Customer.CompanyName+" "+order.Customer.ContactName+ " "+order.OrderDate);
+        #endregion
+
+
+        #region Siparişleri sipariş tarihine göre en yeniden en eski siparişe doğru sıralayarak ekrana yazdırınız.
+
+        //var orders = execute.GetOrders();
+
+        //if (orders.Count > 0)
+        //{
+        //    foreach (var item in orders)
+        //    {
+        //        Console.WriteLine(item.OrderId+" "+item.OrderDate);
+        //    }
+        //}
+
+
+        #endregion
+
+
+        #region Her üründen toplam verilen sipariş ürün adetini ProductId ve Toplam Ürün sayısı olarak listeleyiniz.
+
+        //foreach (var item in execute.ProductGetQuantity())
+        //{
+        //    Console.WriteLine(item.ProductId+":"+item.SumQuantity);
+        //}
+        //Console.WriteLine("------------------");
+
+        //execute.ProductGetQuantity().ForEach(i => Console.WriteLine(i.ProductId + ":" + i.SumQuantity)); //ForeachOneLine
+        #endregion
+
+
+        #region Kendisine categoryId parametresi gelirse o kategorideki ürünleri, gelmezse bütün ürünleri listeleyen LINQ komutunu yazınız.
+
+        execute.ProductsGetByCategory().ForEach(i => Console.WriteLine(i.ProductId + " " + i.ProductName));
+
+        Console.WriteLine("------------------------");
+
+        execute.ProductsGetByCategory(1).ForEach(i => Console.WriteLine(i.ProductId + " " + i.ProductName));
+
+
+        #endregion
+
+
+        /*
+            db.Products.Any();
+            db.Products.Take();
+            db.Products.Skip();
+            db.Products.AsTracking();
+            db.Products.Contains();
+            db.Products.DistinctBy();
+            db.Products.FirstOrDefault() SingleOrDefault()     farkları
+            db.Products.FromSql();
+            db.Products.Join();
+         */
+    }
+
+
 }
 
 
